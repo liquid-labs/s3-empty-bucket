23c97a1b4c0fd05d813a78d2f65a8b4f5c4b9bb8
Zane Rockenbaugh <zane@liquid-labs.com>
2024-03-01 10:30:27 -0600

    provide helpful message on authentication failure

--- src/cli/s3-empty-bucket.mjs
+++ src/cli/s3-empty-bucket.mjs
@@ -10,18 +10,37 @@ const cliSpec = {
   mainOptions : [
     { name : 'bucketName', defaultOption : true, description : 'The name of the bucket to empty.' },
     { name : 'profile', alias : 'p', description : 'The SSO profile to use.' },
-    { name : 'quiet', alias : 'q', type : Boolean, description : 'Suppresses output.' }
+    { name : 'quiet', alias : 'q', type : Boolean, description : 'Suppresses output.' },
+    { 
+      name : 'throw-error', 
+      type: Boolean, 
+      description: 'Instead of printing simple message, allows exceptions to bubble up to the user.'
+    }
   ]
 }
 
-const s3EmptyBucket = () => {
+const s3EmptyBucket = async () => {
   const options = commandLineArgs(cliSpec.mainOptions)
   const { bucketName, profile, quiet } = options
+  const throwError = options['throw-error']
 
-  const credentials = getCredentials({ ssoProfile : profile })
-  const s3Client = new S3Client({ credentials })
+  try {
+    const credentials = getCredentials({ ssoProfile : profile })
+    const s3Client = new S3Client({ credentials })
 
-  emptyBucket({ bucketName, s3Client, verbose : !quiet })
+    await emptyBucket({ bucketName, s3Client, verbose : !quiet })
+  }
+  catch (e) {
+    if (throwError === true) {
+      throw e
+    }
+    else if (e.name === 'CredentialsProviderError') {
+      process.stderr.write("Could not authenticate with AWS. You must either:\n\n1) (preferred) Create a user in the IAM Identity Center and use the command 'aws aws sso login' or 'aws sso login --profile your-profile-name' and excecute 's3-empty-bucket your-bucket-name --profile your-profile-name'.\n2) Generate API keys and place in the '~/.aws/credentials' file.\n\n")
+    }
+    else {
+      process.stderr.write(e.message + '\n')
+    }
+  }
 }
 
 export { s3EmptyBucket }
