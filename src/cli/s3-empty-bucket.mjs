f1264cc2515433bd63521c3e617cc0cdd6361ff8
Zane Rockenbaugh <zane@liquid-labs.com>
2024-03-01 10:30:51 -0600

    lint formatting

--- src/cli/s3-empty-bucket.mjs
+++ src/cli/s3-empty-bucket.mjs
@@ -11,10 +11,10 @@ const cliSpec = {
     { name : 'bucketName', defaultOption : true, description : 'The name of the bucket to empty.' },
     { name : 'profile', alias : 'p', description : 'The SSO profile to use.' },
     { name : 'quiet', alias : 'q', type : Boolean, description : 'Suppresses output.' },
-    { 
-      name : 'throw-error', 
-      type: Boolean, 
-      description: 'Instead of printing simple message, allows exceptions to bubble up to the user.'
+    {
+      name        : 'throw-error',
+      type        : Boolean,
+      description : 'Instead of printing simple message, allows exceptions to bubble up to the user.'
     }
   ]
 }
@@ -29,15 +29,12 @@ const s3EmptyBucket = async () => {
     const s3Client = new S3Client({ credentials })
 
     await emptyBucket({ bucketName, s3Client, verbose : !quiet })
-  }
-  catch (e) {
+  } catch (e) {
     if (throwError === true) {
       throw e
-    }
-    else if (e.name === 'CredentialsProviderError') {
+    } else if (e.name === 'CredentialsProviderError') {
       process.stderr.write("Could not authenticate with AWS. You must either:\n\n1) (preferred) Create a user in the IAM Identity Center and use the command 'aws aws sso login' or 'aws sso login --profile your-profile-name' and excecute 's3-empty-bucket your-bucket-name --profile your-profile-name'.\n2) Generate API keys and place in the '~/.aws/credentials' file.\n\n")
-    }
-    else {
+    } else {
       process.stderr.write(e.message + '\n')
     }
   }
